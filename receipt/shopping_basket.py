f602e1cd224e2cad7bc7df5c26153ebb05c69e44
Marcel Hohn <marcel.hohn@web.de>
2022-01-22 21:09:49 +0100

    Add functionality to shopping_basket
    The shopping basket can calculate the total sales taxes and the
    total price with sales taxes of its items. It prints a receipt to
    the console.

--- receipt/shopping_basket.py
+++ receipt/shopping_basket.py
@@ -4,17 +4,23 @@ from typing import List
 from receipt.item import Item
 
 class ShoppingBasket:
-    def __init__(self) -> None:
-        self._items = []
-
-    def add(self, items: List[Item]) -> None:
-        return
+    def __init__(self, items: List[Item]) -> None:
+        self._items = items
 
     def print(self) -> None:
-        print()
+        for item in self._items:
+            print(item.generate_receipt_text())
+        
+        total_sales_taxes = self.calculate_total_sales_taxes()
+        print(f"Sales Taxes: {total_sales_taxes}")
+        
+        total_price = self.calculate_total_price()
+        print(f"Total: {total_price}")
 
     def calculate_total_sales_taxes(self) -> Decimal:
-        return Decimal("0.00")
+        return sum([item.calculate_sales_tax() for item in self._items])
 
     def calculate_total_price(self) -> Decimal:
-        return Decimal("0.00")
+        return sum([item.calculate_price_w_sales_tax() for item in self._items])
+
+        
