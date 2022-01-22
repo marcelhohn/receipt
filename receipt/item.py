7ed9f3b6803b971fcad1a4a95353a07bf74136c5
Marcel Hohn <marcel.hohn@web.de>
2022-01-22 13:48:16 +0100

    Convert functions for calculation of sales taxes into Item methods

--- receipt/item.py
+++ receipt/item.py
@@ -1,9 +1,21 @@
 from dataclasses import dataclass
 from decimal import Decimal
 
+
 @dataclass(frozen=True)
 class Item:
-   name: str
-   quantity: int
-   shelf_price: Decimal
-   is_imported: bool
+    name: str
+    quantity: int
+    shelf_price: Decimal
+    is_imported: bool
+
+    def calculate_sales_tax(self) -> Decimal:
+        self.calculate_tax_rate()
+        return Decimal("0.0")
+
+    def calculate_tax_rate(self) -> Decimal:
+        self.is_basic_tax_free()
+        return Decimal("0.0")
+
+    def is_basic_tax_free(self) -> bool:
+        return False
