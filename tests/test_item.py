dcbc3d782e0d4cb2a0b47ed158fbb3c09f8daf72
Marcel Hohn <marcel.hohn@web.de>
2022-01-22 22:24:52 +0100

    Delete skipped unit test

--- tests/test_item.py
+++ tests/test_item.py
@@ -21,27 +21,6 @@ class ItemCreation(TestCase):
         self.assertEqual(test_item._shelf_price, test_shelf_price)
         self.assertEqual(test_item._is_imported, test_is_imported)
 
-    @unittest.skip(
-        "unfreeze dataclass since frozen dataclass does not allow to mock mehtods, "
-        "which we need for testing other mehtods of the Item class"
-    )
-    def test_item_does_not_allow_mutation_after_creation(self):
-        test_name = "book"
-        test_quantity = 1
-        test_shelf_price = Decimal("12.49")
-        test_is_imported = False
-
-        test_item = Item(test_name, test_quantity, test_shelf_price, test_is_imported)
-
-        with self.assertRaises(FrozenInstanceError):
-            test_item.name = "CD"
-        with self.assertRaises(FrozenInstanceError):
-            test_item.quantity = 2
-        with self.assertRaises(FrozenInstanceError):
-            test_item.shelf_price = Decimal("0.49")
-        with self.assertRaises(FrozenInstanceError):
-            test_item.is_imported = True
-
 
 class BasicTaxFreeCheck(TestCase):
     def test_book_is_basic_tax_free(self):
