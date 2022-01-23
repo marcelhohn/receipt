e31f4054aa7dec9491bec0646761b3f8dadbfd08
Marcel Hohn <marcel.hohn@web.de>
2022-01-23 15:20:49 +0100

    Refactor tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,14 +1,12 @@
-from dataclasses import FrozenInstanceError
 from decimal import Decimal
 from unittest import TestCase
-import unittest
 from unittest.mock import Mock, patch
 
 from receipt.item import Item
 
 
-class ItemCreation(TestCase):
-    def test_item_is_created_correctly(self):
+class Init(TestCase):
+    def test_creates_item_correctly(self):
         test_name = "book"
         test_quantity = 1
         test_shelf_price = Decimal("12.49")
@@ -22,7 +20,7 @@ class ItemCreation(TestCase):
         self.assertEqual(test_item._is_imported, test_is_imported)
 
 
-class BasicTaxFreeCheck(TestCase):
+class IsBasicTaxFree(TestCase):
     def test_book_is_basic_tax_free(self):
         book = Item("book", 1, Decimal("0.00"), False)
         self.assertTrue(book.is_basic_tax_free())
@@ -50,35 +48,37 @@ class BasicTaxFreeCheck(TestCase):
         self.assertFalse(bottle_of_perfume.is_basic_tax_free())
 
 
-class TaxRateCalculation(TestCase):
-    def test_tax_rate_for_essential_item(self):
-        essential_item = Item("", 1, Decimal("0.00"), False)
-        essential_item.is_basic_tax_free = Mock(return_value=True)
+class CalculateTaxRate(TestCase):
+    def test_works_for_essential_item(self):
+        essential_item = self.create_item(is_essential=True, is_imported=False)
 
         self.assertEqual(essential_item.calculate_tax_rate(), Decimal("0"))
 
-    def test_tax_rate_for_normal_item(self):
-        normal_item = Item("", 1, Decimal("0.00"), False)
-        normal_item.is_basic_tax_free = Mock(return_value=False)
+    def test_works_for_normal_item(self):
+        normal_item = self.create_item(is_essential=False, is_imported=False)
 
         self.assertEqual(normal_item.calculate_tax_rate(), Decimal("0.1"))
 
-    def test_tax_rate_for_imported_essential_item(self):
-        imported_essential_item = Item("", 1, Decimal("0.00"), True)
-        imported_essential_item.is_basic_tax_free = Mock(return_value=True)
+    def test_works_for_imported_essential_item(self):
+        imported_essential_item = self.create_item(is_essential=True, is_imported=True)
 
         self.assertEqual(imported_essential_item.calculate_tax_rate(), Decimal("0.05"))
 
-    def test_tax_rate_for_imported_normal_item(self):
-        imported_normal_item = Item("", 1, Decimal("0.00"), True)
-        imported_normal_item.is_basic_tax_free = Mock(return_value=False)
+    def test_works_for_imported_normal_item(self):
+        imported_normal_item = self.create_item(is_essential=False, is_imported=True)
 
         self.assertEqual(imported_normal_item.calculate_tax_rate(), Decimal("0.15"))
 
+    @staticmethod
+    def create_item(is_essential: bool, is_imported: bool) -> Item:
+        item = Item("", 1, Decimal("0.00"), is_imported)
+        item.is_basic_tax_free = Mock(return_value=is_essential)
+        return item
 
-class SalesTaxCalculation(TestCase):
+
+class CalculateSalesTax(TestCase):
     @patch("receipt.item.tax_utils.round_sales_tax")
-    def test_round_sales_tax_is_called_with_no_tax(self, patch_round_sales_tax):
+    def test_calls_round_sales_tax_with_no_tax(self, patch_round_sales_tax):
         item = Item("", 1, Decimal("12.49"), False)
         item.calculate_tax_rate = Mock(return_value=Decimal("0"))
 
@@ -86,7 +86,7 @@ class SalesTaxCalculation(TestCase):
         patch_round_sales_tax.assert_called_once_with(Decimal("0"))
 
     @patch("receipt.item.tax_utils.round_sales_tax")
-    def test_round_sales_tax_called_is_called_with_correct_raw_sales_tax(
+    def test_calls_round_sales_tax_with_correct_raw_sales_tax(
         self, patch_round_sales_tax
     ):
         item = Item("", 1, Decimal("12.49"), False)
@@ -96,8 +96,8 @@ class SalesTaxCalculation(TestCase):
         patch_round_sales_tax.assert_called_once_with(Decimal("1.8735"))
 
 
-class PriceWithSalesTaxCalculation(TestCase):
-    def test_unimported_essential_items(self):
+class CalcualtePriceWithSalesTax(TestCase):
+    def test_works_for_unimported_essential_items(self):
         self.assertEqual(
             Item("book", 1, Decimal("12.49"), False).calculate_price_w_sales_tax(),
             Decimal("12.49"),
@@ -115,7 +115,7 @@ class PriceWithSalesTaxCalculation(TestCase):
             Decimal("9.75"),
         )
 
-    def test_unimported_normal_items(self):
+    def test_works_for_unimported_normal_items(self):
         self.assertEqual(
             Item("music CD", 1, Decimal("14.99"), False).calculate_price_w_sales_tax(),
             Decimal("16.49"),
@@ -127,7 +127,7 @@ class PriceWithSalesTaxCalculation(TestCase):
             Decimal("20.89"),
         )
 
-    def test_imported_essential_items(self):
+    def test_works_for_imported_essential_items(self):
         self.assertEqual(
             Item(
                 "box of chocolates", 1, Decimal("10.00"), True
@@ -141,7 +141,7 @@ class PriceWithSalesTaxCalculation(TestCase):
             Decimal("11.85"),
         )
 
-    def test_imported_normal_items(self):
+    def test_works_for_imported_normal_items(self):
         self.assertEqual(
             Item(
                 "bottle of perfume", 1, Decimal("47.50"), True
@@ -156,14 +156,14 @@ class PriceWithSalesTaxCalculation(TestCase):
         )
 
 
-class ReceiptTextGeneration(TestCase):
-    def test_generate_receipt_text_works_for_local_good(self):
+class GenerateReceiptText(TestCase):
+    def test_works_for_local_good(self):
         item = Item("bottle of perfume", 1, Decimal("0.00"), False)
         item.calculate_price_w_sales_tax = Mock(return_value=Decimal("42.00"))
 
         self.assertEqual(item.generate_receipt_text(), "1 bottle of perfume: 42.00")
 
-    def test_generate_receipt_text_works_for_imported_good(self):
+    def test_works_for_imported_good(self):
         item = Item("box of chocolates", 1, Decimal("0.00"), True)
         item.calculate_price_w_sales_tax = Mock(return_value=Decimal("0.99"))
 
--- tests/test_tax_utils.py
+++ tests/test_tax_utils.py
@@ -1,19 +1,18 @@
 from decimal import Decimal
-import decimal
 from unittest import TestCase
 
 import receipt.tax_utils as tax_utils
 
 
-class TestTaxRounding(TestCase):
-    def test_round_sales_tax_rounds_up_correctly(self):
+class RoundSalesTax(TestCase):
+    def test_rounds_up_correctly(self):
         self.assertEqual(tax_utils.round_sales_tax(Decimal("0.0501")), Decimal("0.10"))
         self.assertEqual(tax_utils.round_sales_tax(Decimal("1.43")), Decimal("1.45"))
 
-    def test_round_sales_tax_does_not_change_if_multiple_of_rounding_step(self):
+    def test_does_not_change_if_tax_is_multiple_of_rounding_step(self):
         self.assertEqual(tax_utils.round_sales_tax(Decimal("0.05")), Decimal("0.05"))
         self.assertEqual(tax_utils.round_sales_tax(Decimal("0.1000")), Decimal("0.10"))
 
-    def test_round_sales_tax_returns_two_decimals(self):
+    def test_returns_numbers_with_two_decimals(self):
         result_decimals = -tax_utils.round_sales_tax(Decimal("0.2")).as_tuple().exponent
         self.assertEqual(2, result_decimals)
